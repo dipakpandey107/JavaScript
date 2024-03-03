a8eda8c558abc0df3dce6f18363bb92f6481a13b
Dipak Pandey <133677469+dipakpandey107@users.noreply.github.com>
2024-03-03 11:55:31 +0530

    datatypes

--- variable.js
+++ variable.js
@@ -1,6 +1,35 @@
-let message;
-message = "hello";
+// variable decalare
 
-//alert(message); // displays: hello
+// let message;
+// message = "hello";
 
-console.log(message);
+// //alert(message); // displays: hello
+
+// console.log(message);
+
+
+
+
+// let hello = 'Hello world!';
+
+// let message;
+
+// // copy 'Hello world' from hello into message
+// message = hello;
+
+// // now two variables hold the same data
+// alert(hello); // Hello world!
+// alert(message); // Hello world!
+
+
+// if (true) {
+//     let test = true; // use "let"
+//   }
+  
+// console.log(test); // ReferenceError: test is not defined
+
+ if(true){
+    var test = flase;
+
+ }
+ console.log(test);
