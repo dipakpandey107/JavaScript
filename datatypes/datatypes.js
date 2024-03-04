7461dd3c6b92d63bb27ce17195bb8b2d3f222f9e
Dipak Pandey <133677469+dipakpandey107@users.noreply.github.com>
2024-03-04 17:28:11 +0530

    datatypes

--- datatypes/datatypes.js
+++ datatypes/datatypes.js
@@ -8,3 +8,26 @@ console.log( `hello ${"name"}` ); // hello name
 
 // the expression is a variable, embed it
 console.log( `hello ${name}` ); // hello Ilya
+
+/* 
+
+1 . number
+
+2. bigint
+
+3. string
+
+4.boolean
+
+5.null
+
+6.undefined
+
+7.object
+
+console.log(typeof(undefined)) //undefined
+
+console.log(typeof(null)) // null
+
+
+*/
