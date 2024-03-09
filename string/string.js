d280809e8ad2be1224adaef5a283f446a094992e
Dipak Pandey <133677469+dipakpandey107@users.noreply.github.com>
2024-03-09 21:58:21 +0530

    string

--- string/string.js
+++ string/string.js
@@ -1,11 +1,8 @@
-const name ="dipak";
-
+const name = "dipak-paul";
 const name1 = "pandey";
 
-
-console.log(name+name1);
-
-console.log(name[0]);
-
-console.log(name.toUpperCase());
-
+console.log(name + name1); // Concatenates the two strings
+console.log(name[0]); // Accesses the first character of the string
+console.log(name.toUpperCase()); // Converts the string to uppercase
+const newstring = name.substring(0, 4); // Extracts a substring from index 0 to 3 (4 is exclusive)
+console.log(newstring);
