de8f412c7c373c548a4675a3788e6400e6a91db9
Dipak Pandey <133677469+dipakpandey107@users.noreply.github.com>
2024-03-10 00:15:34 +0530

    maths

--- math/math.js
+++ math/math.js
@@ -1,22 +1,31 @@
-const score = 400;
-console.log(score); // Output: 400
+// const score = 400;
+// console.log(score); // Output: 400
 
 
 
-const balanace = new Number(100);
+// const balanace = new Number(100);
 
-console.log(balanace);
+// console.log(balanace);
 
-console.log(balanace.toString().length);
+// console.log(balanace.toString().length);
 
-console.log(balanace.toFixed(2));
-
-
-const on = 123.9099;
-
-console.log(on.toPrecision(5)); //123.9
+// console.log(balanace.toFixed(2));
 
 
-const hundred = 1000000
-console.log(hundred.toLocaleString()); //10,00,000
+// const on = 123.9099;
 
+// console.log(on.toPrecision(5)); //123.9
+
+
+// const hundred = 1000000
+// console.log(hundred.toLocaleString()); //10,00,000
+
+
+
+//++++++++++++++++++++++++++++ Math   ++++++++++++++
+
+console.log(Math.PI);
+console.log(Math.E);
+console.log(Math.SQRT2);
+console.log(Math.SQRT1);
+console.log(Math.pow(8,2));
