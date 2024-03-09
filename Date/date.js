21774c45172fa3228573eb1d320706425bcdeb6f
Dipak Pandey <133677469+dipakpandey107@users.noreply.github.com>
2024-03-10 00:29:42 +0530

    date

---
+++ Date/date.js
@@ -0,0 +1,18 @@
+
+
+// Dates
+
+
+let  date = new Date();
+
+console.log(date.toString());
+
+console.log(date.toDateString());// Data atual no formato: dia, dia do semana
+
+console.log(date.toLocaleString());
+console.log(typeof date);
+
+
+let mycretedate = new Date(23,0,23);
+
+console.log(mycretedate.toDateString());
