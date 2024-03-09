e93bdbb6040ffe5e3ab37391fa9ce40eab394470
Dipak Pandey <133677469+dipakpandey107@users.noreply.github.com>
2024-03-09 22:15:32 +0530

    string

--- string/string.js
+++ string/string.js
@@ -1,8 +1,30 @@
-const name = "dipak-paul";
+//const name = "dipak-paul";
 const name1 = "pandey";
 
-console.log(name + name1); // Concatenates the two strings
-console.log(name[0]); // Accesses the first character of the string
-console.log(name.toUpperCase()); // Converts the string to uppercase
-const newstring = name.substring(0, 4); // Extracts a substring from index 0 to 3 (4 is exclusive)
-console.log(newstring);
+//console.log(name + name1); // Concatenates the two strings
+//console.log(name[0]); // Accesses the first character of the string
+//console.log(name.toUpperCase()); // Converts the string to uppercase
+//const newstring = name.substring(0, 4); // Extracts a substring from index 0 to 3 (4 is exclusive)
+//console.log(newstring);
+
+
+const str1 = "Hello, world!";
+const str2 = 'JavaScript';
+const str3 = `I'm learning ${str2}`;
+
+console.log(str3);  
+
+const greeting = "Hello";
+const name = "Alice";
+//const message = greeting + ", " + name + "!";
+
+// const str = "JavaScript";
+// const length = str.length;
+// console.log(length);
+
+
+
+const str = "Hello";
+const firstChar = str[0];
+const lastChar = str[str.length - 1];
+console.log(`The first char is: ${firstChar} and The Last Char is :${lastChar}`);
