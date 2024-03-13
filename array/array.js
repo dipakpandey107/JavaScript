b1254d55358d33dfbe7b5d8fcf4c3a7a1f05f1a2
Dipak Pandey <133677469+dipakpandey107@users.noreply.github.com>
2024-03-13 11:34:17 +0530

    array

---
+++ array/array.js
@@ -0,0 +1,14 @@
+//array
+
+
+const myarr=['apple','banana','cherry'];
+
+
+// let arr = new  Array(5,4,5,6);
+//       //create an array with 
+// arr.push(8);
+// arr.push(0);
+
+// console.log(arr);
+
+
