32480508d490e1670f2011cfe3303a975303d102
Dipak Pandey <133677469+dipakpandey107@users.noreply.github.com>
2024-03-13 12:07:47 +0530

    changes

--- Date/date.js
+++ Date/date.js
@@ -1,32 +1,32 @@
 
 
-// Dates
+// // Dates
 
 
-let  date = new Date();
+// let  date = new Date();
 
-console.log(date.toString());
+// console.log(date.toString());
 
-console.log(date.toDateString());// Data atual no formato: dia, dia do semana
+// console.log(date.toDateString());// Data atual no formato: dia, dia do semana
 
-console.log(date.toLocaleString());
-console.log(typeof date);
-
-
-let mycretedate = new Date(23,0,23);
-
-console.log(mycretedate.toDateString());
+// console.log(date.toLocaleString());
+// console.log(typeof date);
 
 
-let mytimestamp = Date.now();
-console.log("Timestamp: "+mytimestamp);
+// let mycretedate = new Date(23,0,23);
 
-console.log(Math.floor(mytimestamp/1000));
+// console.log(mycretedate.toDateString());
 
 
-let newdate = new Date()
+// let mytimestamp = Date.now();
+// console.log("Timestamp: "+mytimestamp);
 
-console.log(newdate.getTime());
-console.log(newdate.getFullYear())
-console.log(newdate.getMonth()+1);
-console.log(newdate.getDay());
+// console.log(Math.floor(mytimestamp/1000));
+
+
+// let newdate = new Date()
+
+// console.log(newdate.getTime());
+// console.log(newdate.getFullYear());
+// console.log(newdate.getMonth()+1);
+// console.log(newdate.getDay());
