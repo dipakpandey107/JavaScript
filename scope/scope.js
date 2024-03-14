4087472a51295a42f33d4a6cc2a51b0e27e13a40
Dipak Pandey <133677469+dipakpandey107@users.noreply.github.com>
2024-03-14 10:08:34 +0530

    scope

--- scope/scope.js
+++ scope/scope.js
@@ -7,6 +7,30 @@ if(true){
 const b = 20;
 var c=30;
 }
-console.log(a);
+//console.log(a);
 //console.log(b);
-console.log(c);
+//console.log(c);
+
+
+function one(){
+    const username = "dipak";
+
+    function  two() {
+      const msg = "hello world";
+      console.log(username);
+    }
+    two();
+}
+one();  
+
+
+if(true){
+    const username = "dipak"
+
+    if(username==="dipak"){
+        const website ="whatsapp"
+        console.log(`${username}'s Website is ${website}`);
+    }
+   // console.log(website);
+}
+//console.log(username);
