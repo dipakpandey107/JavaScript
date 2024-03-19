d36822534f6d594f404d62542e5c00c172489727
Dipak Pandey <133677469+dipakpandey107@users.noreply.github.com>
2024-03-19 17:00:06 +0530

    project3

--- Project/digitalClock/script.js
+++ Project/digitalClock/script.js
@@ -1,10 +1,21 @@
 const digital = document.getElementById('clock');
 
+// setInterval(function() {
+//     let date = new Date();
+//     let hours = date.getHours().toString().padStart(2, '0');
+//     let minutes = date.getMinutes().toString().padStart(2, '0');
+//     let seconds = date.getSeconds().toString().padStart(2, '0');
+//     let timeString = `${hours}:${minutes}:${seconds}`;
+//     digital.textContent = timeString;
+// }, 1000);
+
+
+const time = document.getElementById('clock');
+
 setInterval(function() {
     let date = new Date();
-    let hours = date.getHours().toString().padStart(2, '0');
-    let minutes = date.getMinutes().toString().padStart(2, '0');
-    let seconds = date.getSeconds().toString().padStart(2, '0');
-    let timeString = `${hours}:${minutes}:${seconds}`;
-    digital.textContent = timeString;
+    time.innerText = date.toLocaleTimeString();
+    console.log(date);
 }, 1000);
+
+
